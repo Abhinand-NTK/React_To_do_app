6cce39ae54f35b7906ebd3bc4335616fb8773088
AbhinandNtk <abhinadntkm@gamail.com>
2023-10-22 19:27:35 +0530

    commits are modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Swal from 'sweetalert2'
 
 function App() {
+  const [toDos, setTodos] = useState([]);
+  const [toDo, setTodo] = useState('');
+
+  const addTodo = () => {
+    if (toDo.trim() !== '') {
+      const lowercaseToDo = toDo.toLowerCase();
+      const isDuplicate = toDos.some((todo) => todo.text.toLowerCase() === lowercaseToDo);
+      if (!isDuplicate) {
+        setTodos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+        setTodo('');
+      } else {
+        Swal.fire({
+          title: 'Error!',
+          text: 'This task already exists.',
+          icon: 'error',
+          confirmButtonText: 'Add Another'
+        })
+        
+      }
+    }
+  };
+
+  const handleCheckboxChange = (id, status) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, status: !status } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(toDos.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          value={toDo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={addTodo} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        {toDos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <div className="left">
+              <input
+                type="checkbox"
+                checked={todo.status}
+                onChange={() => handleCheckboxChange(todo.id, todo.status)}
+              />
+              <p>{todo.text}</p>
+              {todo.status ? <p  id='task'>Task completed!</p> : null}
+            </div>
+            <div className="right">
+              <i onClick={() => deleteTodo(todo.id)} className="fas fa-times"></i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
